0e5ff6dbf5323d2dd5e4a4309603fa736a4165d8
Miguel <fernandez.s.miguel96@gmail.com>
2020-06-30 09:25:06 +0200

    card-user component creado

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { CardUserComponent } from './components/card-user/card-user.component';
 
 
 
 @NgModule({
-  declarations: [SideBarComponent],
+  declarations: [SideBarComponent, CardUserComponent],
   imports: [
     CommonModule
   ]
