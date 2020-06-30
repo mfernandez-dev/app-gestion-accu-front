dfa0bcabe2fe9fc8a99b0daf575600cd19a58254
Miguel <fernandez.s.miguel96@gmail.com>
2020-06-30 10:02:40 +0200

    implementacion side-nav temporal

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '../layout/layout.module';
+import { HospitalesModule } from '../hospitales/hospitales.module';
+import { AsociadosModule } from '../asociados/asociados.module';
 
 @NgModule({
   declarations: [],
-  imports: [ ],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    HospitalesModule,
+    AsociadosModule],
   exports: []
 })
 
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { CardUserComponent } from './components/card-user/card-user.component';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 
 @NgModule({
   declarations: [SideBarComponent, CardUserComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule
+  ],
+  exports: [
+    SideBarComponent
   ]
 })
 export class LayoutModule { }
