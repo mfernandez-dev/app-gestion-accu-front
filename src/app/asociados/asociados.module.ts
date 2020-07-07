012dce9b6aa720b8dfb3d7ae36737d49781d8466
Miguel <fernandez.s.miguel96@gmail.com>
2020-07-07 10:13:33 +0200

    creacion componente tabla asociados

--- src/app/asociados/asociados.module.ts
+++ src/app/asociados/asociados.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AsociadosRoutingModule } from './asociados-routing.module';
 import { PantallaAsociadosComponent } from './components/pantalla-asociados/pantalla-asociados.component';
+import { TablaAsociadosComponent } from './components/tabla-asociados/tabla-asociados.component';
 
 
 @NgModule({
-  declarations: [PantallaAsociadosComponent],
+  declarations: [PantallaAsociadosComponent, TablaAsociadosComponent],
   imports: [
     CommonModule,
     AsociadosRoutingModule
---
+++ src/app/asociados/components/tabla-asociados/tabla-asociados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-asociados',
+  templateUrl: './tabla-asociados.component.html',
+  styleUrls: ['./tabla-asociados.component.scss']
+})
+export class TablaAsociadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
