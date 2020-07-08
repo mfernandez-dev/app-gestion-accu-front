52f9ea586777a449daff039349237868b45ce1f8
Miguel <fernandez.s.miguel96@gmail.com>
2020-07-08 11:11:39 +0200

    tabla hospitales

---
+++ src/app/hospitales/components/tabla-hospitales/tabla-hospitales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-hospitales',
+  templateUrl: './tabla-hospitales.component.html',
+  styleUrls: ['./tabla-hospitales.component.scss']
+})
+export class TablaHospitalesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/hospitales/hospitales.module.ts
+++ src/app/hospitales/hospitales.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HospitalesRoutingModule } from './hospitales-routing.module';
 import { PantallaHospitalesComponent } from './components/pantalla-hospitales/pantalla-hospitales.component';
+import { TablaHospitalesComponent } from './components/tabla-hospitales/tabla-hospitales.component';
 
 
 @NgModule({
-  declarations: [PantallaHospitalesComponent],
+  declarations: [PantallaHospitalesComponent, TablaHospitalesComponent],
   imports: [
     CommonModule,
     HospitalesRoutingModule
