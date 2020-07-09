b0eca340c85cf72f1d2c79fe7c4be478ec76106a
Miguel <fernandez.s.miguel96@gmail.com>
2020-07-09 09:59:11 +0200

    detalle hospital implementado

--- src/app/hospitales/components/pantalla-detalle-hospital/pantalla-detalle-hospital.component.ts
+++ src/app/hospitales/components/pantalla-detalle-hospital/pantalla-detalle-hospital.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Hospital } from '../../models/hospital';
+import { HospitalesService } from '../../services/hospitales.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pantalla-detalle-hospital',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PantallaDetalleHospitalComponent implements OnInit {
 
-  constructor() { }
+  public hospital: Hospital;
+  public idHospital: string;
+
+  constructor(private hospitalServ: HospitalesService,
+              private route: ActivatedRoute) {
+              }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.idHospital = String(params.hospitalId);
+      this.getHospitalById(this.idHospital);
+    });
   }
 
+  private getHospitalById(id: string) {
+    this.hospitalServ.getHospitalById(id).subscribe((hospital) => this.hospital = hospital);
+  }
 }
--- src/app/hospitales/components/tabla-medicos/tabla-medicos.component.ts
+++ src/app/hospitales/components/tabla-medicos/tabla-medicos.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Medico } from '../../models/medico';
+import { MatTableDataSource } from '@angular/material/table';
+import { Hospital } from '../../models/hospital';
 
 @Component({
   selector: 'app-tabla-medicos',
   templateUrl: './tabla-medicos.component.html',
   styleUrls: ['./tabla-medicos.component.scss']
 })
-export class TablaMedicosComponent implements OnInit {
+export class TablaMedicosComponent implements OnInit, OnChanges {
+
+  @Input() especialistas: Medico[];
+
+  public displayedColumns: string[] = ['nombre', 'consulta', 'email', 'tlf', 'especialidad'];
+  public dataSource: MatTableDataSource<Medico>;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(): void {
+    this.dataSource = new MatTableDataSource<Medico>(this.especialistas);
+  }
 }
