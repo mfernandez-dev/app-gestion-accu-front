ee29c52cf34f6a7a8e4c1df0b4eaa3b4abf54e3a
Miguel <fernandez.s.miguel96@gmail.com>
2020-07-13 19:53:09 +0200

    Formulario edicion sin funcionalidad

--- src/app/asociados/components/edit-asociado-form/edit-asociado-form.component.ts
+++ src/app/asociados/components/edit-asociado-form/edit-asociado-form.component.ts
@@ -1,39 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AsociadosService } from '../../services/asociados.service';
+import { Asociado } from '../../models/asociado';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-edit-asociado-form',
   templateUrl: './edit-asociado-form.component.html',
   styleUrls: ['./edit-asociado-form.component.scss']
 })
-export class EditAsociadoFormComponent implements OnInit {
+export class EditAsociadoFormComponent implements OnInit, OnChanges {
+  @Input() asociado: Asociado;
 
-  formularioEdicion = new FormGroup({
-    nombre: new FormControl(''),
-    apellido1: new FormControl(''),
-    apellido2: new FormControl(''),
-    dni: new FormControl(''),
-    fechaNacimiento: new FormControl(''),
-    fechaAlta: new FormControl(''),
-    calle: new FormControl(''),
-    numero: new FormControl(''),
-    portal: new FormControl(''),
-    piso: new FormControl(''),
-    puerta: new FormControl(''),
-    localidad: new FormControl(''),
-    provincia: new FormControl(''),
-    codigoPostal: new FormControl(''),
-    pais: new FormControl(''),
-    titular: new FormControl(''),
-    modalidadPago: new FormControl(''),
-    iban: new FormControl(''),
-    patologia: new FormControl(''),
-    fechaDiagnostico: new FormControl(''),
-    tratamiento: new FormControl('')
-  });
+  formularioEdicion: FormGroup;
 
-  constructor( private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+              private asociadoServ: AsociadosService) {
 
-  ngOnInit(): void {
   }
+  ngOnInit(): void {
 
+  }
+  ngOnChanges(): void{
+    this.formularioEdicion = this.formBuilder.group({
+      nombre: [this.asociado?.nombre, Validators.required],
+      apellido1: [this.asociado?.apellido1, Validators.required],
+      apellido2: [this.asociado?.apellido2, Validators.required],
+      dni: [this.asociado?.dni, Validators.required],
+      fechaNacimiento: [this.asociado?.fechaNacimiento, Validators.required],
+      fechaAlta: [this.asociado?.fechaAlta, Validators.required],
+      calle: [this.asociado?.direccion.calle, Validators.required],
+      numero: [this.asociado?.direccion.numero, Validators.required],
+      portal: [this.asociado?.direccion.portal, Validators.required],
+      piso: [this.asociado?.direccion.piso, Validators.required],
+      puerta: [this.asociado?.direccion.letra, Validators.required],
+      localidad: [this.asociado?.direccion.localidad, Validators.required],
+      provincia: [this.asociado?.direccion.provincia, Validators.required],
+      codigoPostal: [this.asociado?.direccion.codigoPostal, Validators.required],
+      pais: [this.asociado?.direccion.pais, Validators.required],
+      titular: [this.asociado?.datosBancarios.titular, Validators.required],
+      modalidadPago: [this.asociado?.datosBancarios.modalidadPago, Validators.required],
+      iban: [this.asociado?.datosBancarios.iban, Validators.required],
+      patologia: [this.asociado?.datosMedicos.enfermedad, Validators.required],
+      fechaDiagnostico: [this.asociado?.datosMedicos.fechaDiagnostico, Validators.required],
+      tratamiento: [this.asociado?.datosMedicos.tratamiento, Validators.required]
+    });
+  }
 }
--- src/app/asociados/components/pantalla-editar-asociado/pantalla-editar-asociado.component.ts
+++ src/app/asociados/components/pantalla-editar-asociado/pantalla-editar-asociado.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AsociadosService } from '../../services/asociados.service';
+import { Asociado } from '../../models/asociado';
 
 @Component({
   selector: 'app-pantalla-editar-asociado',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PantallaEditarAsociadoComponent implements OnInit {
 
-  constructor() { }
+  private idAsociado: string;
+  public asociado: Asociado;
+
+  constructor(private asociadoServ: AsociadosService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.idAsociado = String(params.asociadoId);
+      this.getAsociadoById(this.idAsociado);
+    });
   }
 
+  private getAsociadoById(id: string) {
+    this.asociadoServ.getAsociadoById(id).subscribe((asociado) => this.asociado = asociado);
+  }
 }
